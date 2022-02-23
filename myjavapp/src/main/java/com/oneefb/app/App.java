573f18c414e6b3c379ba38f6b0c05ced126168d8
Edgar Enrique Felix Bonilla <efelix@novandi.mx>
2022-02-23 16:43:43 -0600

    Ajuste maven package

---
+++ myjavapp/src/main/java/com/oneefb/app/App.java
@@ -0,0 +1,13 @@
+package com.oneefb.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
